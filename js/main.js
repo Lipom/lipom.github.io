5171deee6688705bcba29e9af377125912a60a70
Lipom <87226897+Lipom@users.noreply.github.com>
2023-03-12 18:41:23 +0800

    左侧边栏优化

--- js/main.js
+++ js/main.js
@@ -1,60 +1,54 @@
 window.onload = function () {
-    // 左侧边栏按钮的显示和隐藏
-    playSidebarBtn()
-    function playSidebarBtn(){
-        var nav = document.querySelector(".naviMain")
-        var btn = document.querySelector(".leftSidebarBtn")
-        var wrap = document.querySelector(".wrapper")
-
-        // 鼠标滑轮事件
-        wrap.onwheel = ()=>{
-            if(!(nav.getBoundingClientRect().top > 0)){
-                btn.style.display = "block"
-            }
-            else{
-                btn.style.display = "none"
-            }
-        }
-        // 
-       
-    }
     // 左侧边栏的显示和隐藏
     playSidebar()
     function playSidebar() {
         var bar = document.querySelector(".sidebar")
         var open_btn = document.querySelector("#menu_btn")
-        var close_btn = document.querySelector("#close_btn")
-        var change_btn = document.querySelector(".leftSidebarBtn")
-        var flag =false;
-        open_btn.onclick=()=>{
-            if(!flag){
-                console.log("侧边栏开启")
-                bar.className="sidebar sidebar_open"
-                flag =true
+        var change_btn = document.querySelector("#leftSidebarBtn")
+        let nav = document.querySelector(".naviMain")
+        var flag = false;
+
+
+        // 主页下隐藏侧边栏(目前缺陷很大)
+        document.body.onwheel = ()=>{
+            if(!(nav.getBoundingClientRect().top <-200)){
+                bar.style.zIndex = 1
             }else{
-                console.log("侧边栏关闭")
-                bar.className="sidebar sidebar_close"
-                flag =false
+                bar.style.zIndex = 10
             }
         }
-        close_btn.onclick=()=>{
-            console.log("侧边栏关闭")
-            bar.className="sidebar sidebar_close"
-            flag =false
-        }
-        change_btn.onclick=()=>{
-            if(!flag){
+        
+        // 菜单按钮
+        open_btn.onclick = () => {
+            if (!flag) {
                 console.log("侧边栏开启")
-                bar.className="sidebar sidebar_open"
-                flag =true
-            }else{
+                bar.className = "sidebar sidebar_open"
+                bar.style.zIndex = 10
+                flag = true
+            } else {
                 console.log("侧边栏关闭")
-                bar.className="sidebar sidebar_close"
-                flag =false
+                bar.className = "sidebar sidebar_close"
+                bar.style.zIndex = 1
+                flag = false
             }
         }
-      
-       
+        // 侧边栏按钮
+        change_btn.onclick = () => {
+            if (!flag) {
+                console.log("侧边栏开启")
+                bar.className = "sidebar sidebar_open"
+                 // 图标旋转
+                 change_btn.className="sidebarBtn open_btn"
+                flag = true
+            } else {
+                console.log("侧边栏关闭")
+                bar.className = "sidebar sidebar_close"
+                change_btn.className="sidebarBtn close_btn"
+                flag = false
+            }
+        }
+
+
     }
     // 动态显示时间
     playTime();
@@ -114,7 +108,7 @@ window.onload = function () {
                 bc_two.className = "item item_one item_close";
                 bc_three.className = "item item_two item_close";
                 bc_four.className = "naviMain naviMain_close";
-                bc_six.className =  "item item_three item_close";
+                bc_six.className = "item item_three item_close";
                 for (var i = 0; i < five_length; i++) {
                     if (i < five_length - item_numb) {
                         bc_five[i].className = "main_font_color_close";
@@ -131,7 +125,7 @@ window.onload = function () {
                 bc_one.className = "content content_open";
                 bc_two.className = "item item_one item_open";
                 bc_three.className = "item item_two item_open";
-                bc_six.className =  "item item_three item_open";
+                bc_six.className = "item item_three item_open";
                 bc_four.className = "naviMain";
                 for (var i = 0; i < five_length; i++) {
                     if (i < five_length - item_numb) {
@@ -147,37 +141,88 @@ window.onload = function () {
     }
     // 右侧边栏事件
     playRightSidebar()
-    function playRightSidebar(){
+    function playRightSidebar() {
         let like = document.querySelector("#likeIcon")
         let set = document.querySelector("#settingIcon")
         let top = document.querySelector("#topIcon")
         let home = document.querySelector("#homeIcon")
         let support = document.querySelector("#supportIcon")
+        // let nav = document.querySelector(".naviMain")
+        let logo = document.querySelector(".logo")
+
         let topFlag = false
         let setFlag = false
 
         // 开启设置
-        set.onclick=()=>{
-            if(!setFlag){
+        set.onclick = () => {
+            // console.log( set.firstChild)
+            // console.log( set.firstElementChild)
+            if (!setFlag) {
                 // 显示home、support
                 console.log("显示home、support")
-                home.style.display="flex"
-                support.style.display="flex"
+                home.style.display = "flex"
+                support.style.display = "flex"
+                set.firstElementChild.innerHTML = "关闭设置"
                 setFlag = true
-            }else{
+            } else {
                 // 隐藏home、support
                 console.log("隐藏home、support")
-                home.style.display="none"
-                support.style.display="none"
+                home.style.display = "none"
+                support.style.display = "none"
+                set.firstElementChild.innerHTML = "开启设置"
                 setFlag = false
             }
         }
 
-        // 置顶
-        // top.onclick =()=>{
-        //     if(!topFlag){
 
-        //     }
-        // }
+        // 页面元素到浏览器视窗顶部的距离
+        // let currentY = footer.getBoundingClientRect().top
+
+        // 置顶
+        // 页面高度
+        let hightWeb = document.body.clientHeight
+        top.onclick = () => {
+
+            if (!topFlag) {
+                console.log("置顶")
+                // 置顶
+                scrollTo(0, 0)
+                // 图标旋转
+
+                top.firstElementChild.innerHTML = "返回底部"
+                top.lastElementChild.className = "imgRotate"
+                topFlag = true
+            } else {
+                console.log("置底")
+                // 置底
+                scrollTo(0, hightWeb)
+                // 图标旋转
+                top.firstElementChild.innerHTML = "返回顶部"
+                top.lastElementChild.className = ""
+                topFlag = false
+            }
+        }
+        // 自动旋转图标置顶
+        let logoh = logo.offsetHeight
+        console.log(logoh)
+        document.onwheel = () => {
+            console.log("logo距离顶部的距离：" + logo.getBoundingClientRect().top)
+            if ((logo.getBoundingClientRect().top < 0)&&(logo.getBoundingClientRect().top > -400)) {
+                // 旋转
+                top.firstElementChild.innerHTML = "返回底部"
+                top.lastElementChild.className = "imgRotate"
+                topFlag = false
+            }
+            else{
+                if(logo.getBoundingClientRect().top < -1000)
+                {
+                    top.firstElementChild.innerHTML = "返回顶部"
+                    top.lastElementChild.className = ""
+                    topFlag = true
+                }
+               
+            }
+        }
+
     }
 }
